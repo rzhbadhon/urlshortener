eb15cee33eb7530840a690e86e9d6b15f3b44c70
rzhbadhon <rhbadhon030@gmail.com>
2025-11-11 23:23:29 +0600

    finally started this prjct

---
+++ cmd/serve.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Server() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server running")
+	},
+	)
+
+	err := http.ListenAndServe(":5000", mux)
+
+	if err != nil {
+		fmt.Println("Error starting the server", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "github.com/rzhbadhon/urlshortener/cmd"
 
 func main() {
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintln(w, "Server running")
-	},
-
-)
-
-	err := http.ListenAndServe(":5000", mux)
-
-	if err != nil {
-		fmt.Println("Error starting the server", err)
-	}
-
-
+	cmd.Server()
 }
